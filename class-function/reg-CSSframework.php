1d0ca8b14fdb03fd258c217b6a7fc6d61ac0a419
Boien Lim Reyes <mrboyenrey@gmail.com>
2024-05-01 15:39:49 +0800

    Updating Latest CDN

--- class-function/reg-CSSframework.php
+++ class-function/reg-CSSframework.php
@@ -3,21 +3,20 @@
 
 // Enqueue Bootstrap CSS
 function enqueue_bootstrap_css() {
-    wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css', array(), '5.2.0', 'all');
+    wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css', array(), '5.3.3', 'all');
 }
 
 add_action('wp_enqueue_scripts', 'enqueue_bootstrap_css');
 
 // Enqueue Bootstrap JS and Popper.js (required for Bootstrap's JavaScript)
-function enqueue_bootstrap_js() {
+function enqueue_custom_scripts() {
     // Enqueue Popper.js
-    wp_enqueue_script('popper', 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js', array('jquery'), '2.9.3', true);
+    wp_enqueue_script( 'popper-js', 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js', array(), null, true );
 
     // Enqueue Bootstrap JS
-    wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js', array('jquery', 'popper'), '5.2.0', true);
+    wp_enqueue_script( 'bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js', array('popper-js'), null, true );
 }
-
-add_action('wp_enqueue_scripts', 'enqueue_bootstrap_js');   
+add_action( 'wp_enqueue_scripts', 'enqueue_custom_scripts' );
 
 
 
